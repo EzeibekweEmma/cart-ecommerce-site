dfb9e8a167d691f0b0172fed43cee47bb5da847c
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-08-31 18:27:41 +0100

    Added fake api to display product information's

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -1,8 +1,10 @@
+import { fakeApi } from "../../components/product/fakeProductApi";
+
 const productSlice = (set, get) => ({
     product: [],
     fetchProduct: async () => {
-        const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
-        set({ product: await res.json() });
+        // get data from the fakeApi and setting it as product data
+        set({ product: [...fakeApi] });
     },
 });
 

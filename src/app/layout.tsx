c599a14fe8ca28ceb17bcd656e842130626834f6
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2024-10-15 04:10:18 +0100

    Remove unused Home component and update product favorite button styling

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export const metadata = {
+  title: 'CART - Online Shopping Store',
+  description: 'Created with Next.js and Tailwind CSS By Ezeibekwe Emmanuel',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="bg-cGray w-screen overflow-x-hidden">
+      <body className="flex flex-col justify-between min-h-screen">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
